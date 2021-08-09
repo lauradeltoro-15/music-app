38761b87360b5513a45d103dc760d43cf5f8b79c
Laura del Toro <laura.deltoro@seddi.com>
2021-08-09 09:05:08 +0100

    Create common card component

---
+++ src/components/commonComponents/Card/models.ts
@@ -0,0 +1,12 @@
+export interface CardProps {
+  title: string;
+  children: React.ReactNode;
+  height: number;
+  width: number;
+  imageUrl: string;
+}
+
+export interface StyledCardProps {
+  height: number;
+  width: number;
+}
