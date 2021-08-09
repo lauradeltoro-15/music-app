539b68bd1f059a71daabfdf827231f8242c635f5
Laura del Toro <laura.deltoro@seddi.com>
2021-08-09 10:08:15 +0100

    Create default page and music list page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes";
 import { cardListMock } from "../mocks/cardListMock";
+import { Route, Switch } from "react-router-dom";
+
 import CardList from "./commonComponents/CardList";
+import HomePage from "./pages/HomePage";
+import MusicListPage from "./pages/MusicListPage";
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <CardList items={cardListMock}></CardList>
+      <Switch>
+        <Route exact path="/" render={() => <HomePage />} />
+        <Route exact path="/music-list" render={() => <MusicListPage />} />
+      </Switch>
     </ThemeProvider>
   );
 };
