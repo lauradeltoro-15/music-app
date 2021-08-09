ac8c4f4235433df9f72b0ebe4acffee40344f4da
Laura del Toro <laura.deltoro@seddi.com>
2021-08-09 09:40:07 +0100

    Create cardList component

--- src/components/commonComponents/Card/index.tsx
+++ src/components/commonComponents/Card/index.tsx
@@ -1,7 +1,13 @@
 import { CardDescription, CardImage, CardTitle, StyledCard } from "./layout";
 import { CardProps } from "./models";
 
-const Card = ({ title, children, height, width, imageUrl }: CardProps) => {
+const Card = ({
+  title,
+  children,
+  height = 300,
+  width = 300,
+  imageUrl,
+}: CardProps) => {
   return (
     <StyledCard width={width} height={height}>
       <CardImage src={imageUrl} height={height}></CardImage>
---
+++ src/components/commonComponents/CardList/layout.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledList = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+`;
