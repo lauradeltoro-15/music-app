6a29525c8fc01110c9b02128f0afb72863bf73b2
Laura del Toro <laura.deltoro@seddi.com>
2021-09-08 22:08:34 +0100

    Card test component

--- src/components/commonComponents/Card/index.tsx
+++ src/components/commonComponents/Card/index.tsx
@@ -1,14 +1,25 @@
 import { CardDescription, CardImage, CardTitle, StyledCard } from "./layout";
-import { CardProps } from "./models";
+import { CardProps, CardStyle } from "./models";
+
+export const defaultStyle: CardStyle = {
+  width: 300,
+  height: 500,
+  margin: 15,
+  imageHeight: 300,
+};
 
 const Card = ({ style, data: { title, children, imageUrl } }: CardProps) => {
   return (
     <StyledCard
-      width={style?.width || 300}
-      height={style?.height || 500}
-      margin={style?.margin || 15}
+      width={style?.width || defaultStyle.width}
+      height={style?.height || defaultStyle.height}
+      margin={style?.margin || defaultStyle.margin}
+      data-testid="card"
     >
-      <CardImage src={imageUrl} height={style?.imageHeight || 300}></CardImage>
+      <CardImage
+        src={imageUrl}
+        height={style?.imageHeight || defaultStyle.imageHeight}
+      ></CardImage>
       <CardTitle>{title}</CardTitle>
       <CardDescription>{children}</CardDescription>
     </StyledCard>
---
+++ src/components/tests/testSampleData.tsx
@@ -0,0 +1,15 @@
+export const testUrl = "https://www.example.com";
+export const TestDescription = () => <p>Card Descrition Example</p>;
+
+export const cardStyle = {
+  height: 600,
+  width: 250,
+  imageHeight: 250,
+  margin: 10,
+};
+
+export const cardData = {
+  title: "Card title test",
+  children: <TestDescription />,
+  imageUrl: testUrl,
+};
