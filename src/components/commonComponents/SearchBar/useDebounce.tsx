4b6aec17232fc08285136b979fe4bed9d91099a8
Laura del Toro <laura.deltoro@seddi.com>
2021-09-12 09:57:49 +0100

    useDebounce tests

--- src/components/commonComponents/SearchBar/useDebounce.tsx
+++ src/components/commonComponents/SearchBar/useDebounce.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 
-const useDebounce = <T, >(value: T, delay?: number): T =>  {
+export const DEFAULT_DELAY = 500;
+
+const useDebounce = <T,>(value: T, delay?: number): T => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    const timer = setTimeout(
+      () => setDebouncedValue(value),
+      delay || DEFAULT_DELAY
+    );
 
     return () => {
       clearTimeout(timer);
@@ -12,6 +17,6 @@ const useDebounce = <T, >(value: T, delay?: number): T =>  {
   }, [value, delay]);
 
   return debouncedValue;
-}
+};
 
 export default useDebounce;
